14aa8a703bcb4dd66d7c93221ed05f37de099047
Hao Liu <haoliu@thoughtworks.com>
2018-06-12 23:45:14 +0800

    generator in python

---
+++ python_work/pymain/ch09/generator_test.py
@@ -0,0 +1,22 @@
+# 不迭代类似于字符串的对象:
+def flatten(nested):
+    result = []
+    try:
+        try:
+            nested + ''
+        except TypeError:
+            pass
+        else:  # 未发生异常时
+            raise TypeError
+        for sublist in nested:
+            for element in flatten(sublist):
+                result.append(element)
+    except TypeError:
+        result.append(nested)
+    return result
+
+
+def repeater(value):
+    while True:
+        new = (yield value)
+        if new is not None: value = new
