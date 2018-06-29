173e478977a8c99c5d1e2331fa209f5089e420cd
haoliu <haoliu@thoughtworks.com>
2018-06-29 09:15:46 +0800

    Decorator for class

--- python_work/pyadvance/decorator/decorator_class.py
+++ python_work/pyadvance/decorator/decorator_class.py
@@ -16,6 +16,21 @@ def bar():
     print('bar')
 
 
+class NullDecl(object):
+    def __init__(self, func):
+        self.func = func
+        for name in set(dir(func)) - set(dir(self)):
+            setattr(self, name, getattr(func, name))
+
+    def __call__(self, *args):
+        return self.func(*args)
+
+
+@NullDecl
+def my_func(x, y, z):
+    return (x + y) / z
+
+
 if __name__ == '__main__':
     bar()
-    f(2)
+    print(my_func(1, 2, 3))
