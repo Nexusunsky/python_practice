cecec8476454277f132a05b1fc314a93b4b383bc
Hao Liu <haoliu@thoughtworks.com>
2018-07-12 23:23:21 +0800

    Future demo of downlaod flags

---
+++ python_work/pyadvance/multithread/nginxdemo/flags_threadpoll_fl.py
@@ -0,0 +1,59 @@
+"""Download flags of top 20 countries by population
+ThreadPoolExecutor version
+Sample run::
+    $ python3 flags_threadpool.py
+    BD retrieved.
+    EG retrieved.
+    CN retrieved.
+    ...
+    PH retrieved.
+    US retrieved.
+    IR retrieved.
+    20 flags downloaded in 0.93s
+"""
+# BEGIN FLAGS_THREADPOOL
+from concurrent import futures
+
+from pyadvance.multithread.nginxdemo.flags_fl import get_flag, show, save_flag, main
+
+MAX_WORKERS = 20  # <2>
+
+
+def download_one(cc):  # <3>
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + '.gif')
+    return cc
+
+
+def download_many(cc_list):
+    workers = min(MAX_WORKERS, len(cc_list))  # <4>
+    with futures.ThreadPoolExecutor(workers) as executor:  # <5>
+        res = executor.map(download_one, sorted(cc_list))  # <6>
+
+    return len(list(res))  # <7>
+
+
+def download_many_ac(cc_list):
+    cc_list = cc_list[:5]
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        to_do = []
+        for cc in sorted(cc_list):
+            future = executor.submit(download_one, cc)
+            to_do.append(future)
+            msg = 'Schedule for {}: {}'
+            print(msg.format(cc, future))
+
+        results = []
+        for future in futures.as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {!r}'
+            print(msg.format(future, res))
+            results.append(res)
+
+    return len(results)
+
+
+if __name__ == '__main__':
+    main(download_many_ac)  # <8>
+# END FLAGS_THREADPOOL
